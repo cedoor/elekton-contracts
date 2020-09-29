05d2297cc694852f434157a54b238e953aff9a2e
cedoor <me@cedoor.dev>
2020-09-29 20:04:21 +0200

    [test] add snark program test

--- test/sample-test.js
+++ test/sample-test.js
@@ -1,14 +1,49 @@
 const { expect } = require("chai")
-const { bytes32ToString, stringToBytes32, deploy } = require("../scripts/utils")
+const {
+	bytes32ToString,
+	stringToBytes32,
+	deployContract,
+	getAccountAddresses,
+	getAccounts,
+	getPublicKeyHex,
+	createAccount,
+	mod
+} = require("../scripts/utils")
+const { MerkleTree } = require("merkletreejs")
+const SHA256 = require("crypto-js/sha256")
+const path = require("path")
+const fs = require("fs")
+const { initialize } = require("zokrates-js/node")
+const { config } = require(path.resolve("package.json"))
 
 describe("Elekton", function () {
-	it("Should return the user data once an admin is created", async function () {
-		const elekton = await deploy("Elekton")
+	let zokrates
+	let elektonInstance
 
-		await elekton.createAdmin(...bytes32ToString(["Pinco", "Pallino", "pallino"]))
-
-		const userData = await elekton.getUser()
-
-		expect(stringToBytes32(userData.name)).to.equal("Pinco")
+	before(async function () {
+		zokrates = await initialize()
+		elektonInstance = await deployContract("Elekton")
 	})
+
+	it("Snark program should work fine", async function () {
+		const { privateKey, publicKey } = createAccount()
+
+		const artifacts = JSON.parse(fs.readFileSync(path.join(config.paths.build.zksnark, "artifacts.json"), "utf-8"))
+
+		const { output } = zokrates.computeWitness(artifacts, [privateKey])
+
+		expect(publicKey).to.deep.equal(JSON.parse(output)[0])
+	})
+
+	// it("An admin should create an election with a list of user public keys (elector addresses)", async function () {
+	// 	const elekton = await deployContract("Elekton")
+	//
+	// 	const electors = (await getAccountAddresses()).slice(0, 8)
+	//
+	// 	elekton.createElection("test", stringToBytes32(electors))
+	//
+	// 	const election = await elekton.getElection("test")
+	//
+	// 	expect(bytes32ToString(election.electors)).to.deep.equal(electors)
+	// })
 })
