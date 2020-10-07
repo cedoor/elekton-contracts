98bbdb7662d515d3093444a5c1c7e156aceb594f
cedoor <me@cedoor.dev>
2020-10-07 17:21:57 +0200

    [feat] create base functions to create elections and vote anonymously

--- contracts/Elekton.sol
+++ contracts/Elekton.sol
@@ -18,8 +18,14 @@ contract Elekton is Ownable, Verifier {
         bool isAdmin;
     }
 
+    struct Election {
+        bytes32[] electors;
+        uint[] votes;
+    }
+
     mapping(address => User) private users;
     mapping(bytes32 => bool) private usernames;
+    mapping(uint => Election) private elections;
 
     function createAdmin(bytes32 _name, bytes32 _surname, bytes32 _username) external {
         createUser(_name, _surname, _username, true, _msgSender());
@@ -48,6 +54,26 @@ contract Elekton is Ownable, Verifier {
         usernames[_username] = true;
 
         emit UserCreated(_address);
+    }
+
+    function createElection(uint _id, bytes32[] calldata _electors) external {
+        Election storage election = elections[_id];
+
+        election.electors = _electors;
+    }
+
+    function getElection(uint _id) external view returns (bytes32[] memory electors, uint[] memory votes) {
+        Election storage election = elections[_id];
+
+        return (election.electors, election.votes);
+    }
+
+    function vote(uint[2] memory a, uint[2][2] memory b, uint[2] memory c, uint[4] memory input) external {
+        require(verifyProof(a, b, c, input), "invalid-proof");
+
+        Election storage election = elections[input[2]];
+
+        election.votes.push(input[1]);
     }
 
 }
