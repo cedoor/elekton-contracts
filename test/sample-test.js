bc7c8f20128d79e0d2688c012ddc35f812bdc3bc
cedoor <me@cedoor.dev>
2020-10-07 17:20:42 +0200

    [test] add tests for election creaction and anonymous vote

--- test/sample-test.js
+++ test/sample-test.js
@@ -1,30 +1,42 @@
 const { expect } = require("chai")
-const { deployContract, createProof, createAccount } = require("../scripts/utils")
+const { babyJub } = require("circomlib")
+const { deployContract, createAccounts, createElektonProof } = require("../scripts/utils")
 
 describe("Elekton", function () {
 	let elektonInstance
+	let accounts
 
 	before(async function () {
 		elektonInstance = await deployContract("Elekton")
+		accounts = await createAccounts(4)
 	})
 
-	it("Snark program should work fine", async function () {
-		const { privateKey, publicKey } = await createAccount()
-		const proof = await createProof({ in: privateKey, Ax: publicKey[0], Ay: publicKey[1] })
-		const proofValidity = await elektonInstance.verifyProof(...proof)
+	it("An admin should create an election with a list of enabled electors", async function () {
+		const electors = accounts.map((account) => `0x${babyJub.packPoint(account.publicKey).toString("hex")}`)
 
-		expect(proofValidity).to.equal(true)
+		await elektonInstance.createElection(2, electors)
+
+		const election = await elektonInstance.getElection(2)
+
+		expect(election.electors).to.deep.equal(electors)
 	})
 
-	// it("An admin should create an election with a list of user public keys (elector addresses)", async function () {
-	// 	const elekton = await deployContract("Elekton")
-	//
-	// 	const electors = (await getAccountAddresses()).slice(0, 8)
-	//
-	// 	elekton.createElection("test", stringToBytes32(electors))
-	//
-	// 	const election = await elekton.getElection("test")
-	//
-	// 	expect(bytes32ToString(election.electors)).to.deep.equal(electors)
-	// })
+	it("An elector should vote anonymously", async function () {
+		const electionId = 2
+		const vote = 4n
+		const proof = await createElektonProof(
+			accounts.map((account) => account.publicKey),
+			electionId,
+			accounts[2],
+			vote
+		)
+
+		await elektonInstance.vote(...proof)
+
+		const election = await elektonInstance.getElection(2)
+
+		console.log(election)
+
+		expect(election.votes[0]).to.equal(vote)
+	})
 })
