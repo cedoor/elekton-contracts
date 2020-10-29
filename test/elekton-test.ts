6fa5ef23e53db63badb71d74e12c48bf03e20440
cedoor <me@cedoor.dev>
2020-10-29 22:18:43 +0100

    [test] update contract parameters

--- test/elekton-test.ts
+++ test/elekton-test.ts
@@ -1,101 +1,68 @@
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/dist/src/signer-with-address"
 import { expect } from "chai"
-import { babyJub } from "circomlib"
 import { Contract } from "ethers"
-import {
-	deployContract,
-	attachContract,
-	createVoterAccounts,
-	createElektonProof,
-	getSmt,
-	getEthereumAccounts,
-	stringToBytes32
-} from "../scripts/utils"
+import { createElektonProof, createVoterAccounts, deployContract, getEthereumAccounts, getSmt } from "../scripts/utils"
 
 describe("Elekton", function () {
 	let elekton: Contract
-	let admin: any
-	let ballot: any
+	let ethereumAccounts: SignerWithAddress[]
 	let voterAccounts: any
 
 	before(async function () {
-		const ethereumAccounts = await getEthereumAccounts()
+		ethereumAccounts = await getEthereumAccounts()
 		elekton = await deployContract("Elekton")
-		admin = elekton.connect(ethereumAccounts[1])
 		voterAccounts = createVoterAccounts(5)
 	})
 
-	it("An admin should add himself to elekton contract", async function () {
-		await admin.createAdmin()
+	it("An user should add himself to elekton contract", async function () {
+		const address = await ethereumAccounts[1].getAddress()
+		const id = 121n
 
-		const adminAddress = await admin.signer.getAddress()
-		const isAdmin = await admin.isAdmin(adminAddress)
-
-		expect(isAdmin).to.deep.equal(true)
+		await expect(elekton.connect(ethereumAccounts[1]).createUser(id))
+			.to.emit(elekton, "UserCreated")
+			.withArgs(address, id)
 	})
 
-	it("A voter should add himself to elekton contract", async function () {
-		const publicKey = `0x${babyJub.packPoint(voterAccounts[0].publicKey).toString("hex")}`
-		const username = stringToBytes32("pincopallino")
-		const name = stringToBytes32("Pinco")
-		const surname = stringToBytes32("Pallino")
-
-		await elekton.createVoter(publicKey, username, name, surname)
-
-		const voter = await elekton.voters(0)
-
-		expect(voter.username).to.deep.equal(username)
-	})
-
-	it("An admin should create a ballot", async function () {
-		const name = stringToBytes32("Referendum")
-		const question = stringToBytes32("What do you want to vote for?")
-		const proposals = stringToBytes32(["Yes", "No"])
-		const voters = [
-			`0x${babyJub.packPoint(voterAccounts[0].publicKey).toString("hex")}`,
-			`0x${babyJub.packPoint(voterAccounts[1].publicKey).toString("hex")}`
-		]
+	it("An user should create a ballot", async function () {
+		const id = 12n
 		const smt = await getSmt([voterAccounts[0].publicKey, voterAccounts[1].publicKey])
-		const smtRoot = `0x${smt.root.toString("16")}`
 		const startDate = Math.floor(Date.now() / 1000) - 10
 		const endDate = Math.floor(Date.now() / 1000) + 10
-		const encryptionKey = stringToBytes32("key")
 
-		await admin.createBallot(name, question, proposals, voters, smtRoot, startDate, endDate, encryptionKey)
-
-		const ballotAddress = await admin.ballots(0)
-		ballot = await attachContract("Ballot", ballotAddress)
-
-		expect(await ballot.name()).to.deep.equal(name)
+		await expect(elekton.connect(ethereumAccounts[1]).createBallot(id, smt.root, startDate, endDate))
+			.to.emit(elekton, "BallotCreated")
+			.withArgs(id)
 	})
 
 	it("A voter of a ballot should vote anonymously in that ballot", async function () {
-		const ballotAddress = BigInt(ballot.address)
+		const id = 12n
 		const vote = 2n
 		const proof = await createElektonProof(
 			[voterAccounts[0].publicKey, voterAccounts[1].publicKey],
-			ballotAddress,
+			id,
 			voterAccounts[0],
 			vote
 		)
 
-		await ballot.vote(...proof)
-
-		expect(await ballot.votes(0)).to.deep.equal(vote)
+		await expect(elekton.vote(...proof))
+			.to.emit(elekton, "VoteAdded")
+			.withArgs(id, vote)
 	})
 
-	it("An admin should set the poll key when the voting ends", async function () {
-		function delay(duration = 10000) {
+	it("A ballot admin should set the poll key when the voting ends", async function () {
+		function delay(duration = 5000) {
 			return new Promise((resolve) => {
 				setTimeout(resolve, duration)
 			})
 		}
 
 		await delay()
-		const b = ballot.connect(admin.signer)
-		const pollKey = stringToBytes32("key")
 
-		await b.setPollKey(pollKey)
+		const id = 12n
+		const pollKey = 122n
 
-		expect(await ballot.pollKey()).to.deep.equal(pollKey)
+		await expect(elekton.connect(ethereumAccounts[1]).publishPollKey(id, pollKey))
+			.to.emit(elekton, "PollKeyPublished")
+			.withArgs(id, pollKey)
 	})
 })
