07cd12995b4f41ac79c4ac49d82badee659c8bf4
cedoor <me@cedoor.dev>
2020-11-07 13:30:06 +0100

    [test] add some tests for createUser function

--- test/core-test.ts
+++ test/core-test.ts
@@ -12,46 +12,74 @@ describe("Core tests", function () {
 	})
 
 	describe("#createUser()", function () {
-		for (let i = 1; i < 5; i++) {
-			it(`User ${i} should add themselves to elekton contract`, async function () {
-				const address = await accounts[i].signer.getAddress()
+		it(`An user with id = 0 should not add himself`, async function () {
+			const elektonUser = elekton.connect(accounts[1].signer)
 
-				await expect(elekton.connect(accounts[i].signer).createUser(i))
-					.to.emit(elekton, "UserCreated")
-					.withArgs(address, i)
+			await expect(elektonUser.createUser(0)).to.be.revertedWith("user-id-cannot-be-0")
+		})
+
+		for (let userId = 1; userId < 5; userId++) {
+			it(`User with id = ${userId} should add himself`, async function () {
+				const elektonUser = elekton.connect(accounts[userId].signer)
+				const address = await accounts[userId].signer.getAddress()
+
+				await expect(elektonUser.createUser(userId)).to.emit(elekton, "UserCreated").withArgs(address, userId)
 			})
 		}
+
+		it(`User with id = 1 should not update his id to 1`, async function () {
+			const elektonUser = elekton.connect(accounts[1].signer)
+
+			await expect(elektonUser.createUser(1)).to.be.revertedWith("user-id-cannot-be-same")
+		})
+
+		it(`User with id = 1 should update his id to 5`, async function () {
+			const elektonUser = elekton.connect(accounts[1].signer)
+			const address = await accounts[1].signer.getAddress()
+			const userId = 5
+
+			await expect(elektonUser.createUser(userId)).to.emit(elekton, "UserUpdated").withArgs(address, userId)
+		})
+
+		it(`User with id = 5 should update again his id to 1`, async function () {
+			const elektonUser = elekton.connect(accounts[1].signer)
+			const address = await accounts[1].signer.getAddress()
+			const userId = 1
+
+			await expect(elektonUser.createUser(userId)).to.emit(elekton, "UserUpdated").withArgs(address, userId)
+		})
 	})
 
 	describe("#createBallot()", function () {
-		it("User 0 should create a ballot with 4 users as voters", async function () {
-			const id = 12n
+		it("User with id = 1 should create a ballot of 30s with 4 voters", async function () {
+			const elektonUser = elekton.connect(accounts[1].signer)
+			const ballotId = 12n
 			const smt = await getSmt(accounts.slice(1, 5).map((account) => account.voter.publicKey))
-			const startDate = Math.floor(Date.now() / 1000) - 10
-			const endDate = Math.floor(Date.now() / 1000) + 25
+			const startDate = Math.floor(Date.now() / 1000)
+			const endDate = Math.floor(Date.now() / 1000) + 30
 
-			await expect(elekton.connect(accounts[1].signer).createBallot(id, smt.root, startDate, endDate))
+			await expect(elektonUser.createBallot(ballotId, smt.root, startDate, endDate))
 				.to.emit(elekton, "BallotCreated")
-				.withArgs(id)
+				.withArgs(ballotId)
 		})
 	})
 
 	describe("#vote()", function () {
-		const id = 12n
+		const ballotId = 12n
 
-		for (let i = 1; i < 5; i++) {
-			it(`Authorized user ${i} should vote anonymously in a ballot`, async function () {
-				const vote = BigInt(i % 2)
+		for (let userId = 1; userId < 5; userId++) {
+			it(`Voter with id = ${userId} should vote anonymously in a ballot`, async function () {
+				const vote = BigInt(userId % 2)
 				const proof = await createElektonProof(
 					accounts.slice(1, 5).map((account) => account.voter.publicKey),
-					id,
-					accounts[i].voter,
+					ballotId,
+					accounts[userId].voter,
 					vote
 				)
 
 				await expect(elekton.vote(...proof))
 					.to.emit(elekton, "VoteAdded")
-					.withArgs(id, vote)
+					.withArgs(ballotId, vote)
 			})
 		}
 	})
