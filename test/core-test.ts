31574d68d3edc77212a6f72c64bd5e63426bd775
cedoor <me@cedoor.dev>
2020-11-08 19:45:50 +0100

    [test] add some tests for createBallot function

--- test/core-test.ts
+++ test/core-test.ts
@@ -12,53 +12,79 @@ describe("Core tests", function () {
 	})
 
 	describe("#createUser()", function () {
-		it(`An user with id = 0 should not add himself`, async function () {
-			const elektonUser = elekton.connect(accounts[1].signer)
+		let elektonUser1: Contract
+		let user1Address: string
 
-			await expect(elektonUser.createUser(0)).to.be.revertedWith("E000")
+		before(async function () {
+			elektonUser1 = elekton.connect(accounts[1].signer)
+			user1Address = await accounts[1].signer.getAddress()
 		})
 
 		for (let userId = 1; userId < 5; userId++) {
 			it(`User with id = ${userId} should add himself`, async function () {
 				const elektonUser = elekton.connect(accounts[userId].signer)
-				const address = await accounts[userId].signer.getAddress()
+				const userAddress = await accounts[userId].signer.getAddress()
 
-				await expect(elektonUser.createUser(userId)).to.emit(elekton, "UserCreated").withArgs(address, userId)
+				await expect(elektonUser.createUser(userId))
+					.to.emit(elekton, "UserCreated")
+					.withArgs(userAddress, userId)
 			})
 		}
 
-		it(`User with id = 1 should not update his id to 1`, async function () {
-			const elektonUser = elekton.connect(accounts[1].signer)
+		it(`An user with id = 0 should not add himself`, async function () {
+			await expect(elektonUser1.createUser(0)).to.be.revertedWith("E000")
+		})
 
-			await expect(elektonUser.createUser(1)).to.be.revertedWith("E001")
+		it(`User with id = 1 should not update his id to 1`, async function () {
+			await expect(elektonUser1.createUser(1)).to.be.revertedWith("E001")
 		})
 
 		it(`User with id = 1 should update his id to 5`, async function () {
-			const elektonUser = elekton.connect(accounts[1].signer)
-			const address = await accounts[1].signer.getAddress()
-			const userId = 5
-
-			await expect(elektonUser.createUser(userId)).to.emit(elekton, "UserUpdated").withArgs(address, userId)
+			await expect(elektonUser1.createUser(5)).to.emit(elekton, "UserUpdated").withArgs(user1Address, 5)
 		})
 
 		it(`User with id = 5 should update again his id to 1`, async function () {
-			const elektonUser = elekton.connect(accounts[1].signer)
-			const address = await accounts[1].signer.getAddress()
-			const userId = 1
-
-			await expect(elektonUser.createUser(userId)).to.emit(elekton, "UserUpdated").withArgs(address, userId)
+			await expect(elektonUser1.createUser(1)).to.emit(elekton, "UserUpdated").withArgs(user1Address, 1)
 		})
 	})
 
 	describe("#createBallot()", function () {
+		let elektonUser1: Contract
+		let ballotId: bigint
+		let smt: any
+
+		before(async function () {
+			elektonUser1 = elekton.connect(accounts[1].signer)
+			ballotId = 12n
+			smt = await getSmt(accounts.slice(1, 5).map((account) => account.voter.publicKey))
+		})
+
+		it("An unregistered user should not create a ballot", async function () {
+			const startDate = Math.floor(Date.now() / 1000) + 12
+			const endDate = Math.floor(Date.now() / 1000) + 100
+
+			await expect(elekton.createBallot(ballotId, smt.root, startDate, endDate)).to.be.revertedWith("E100")
+		})
+
+		it("A ballot should not start in the past", async function () {
+			const startDate = Math.floor(Date.now() / 1000) - 10
+			const endDate = Math.floor(Date.now() / 1000) + 1000
+
+			await expect(elektonUser1.createBallot(ballotId, smt.root, startDate, endDate)).to.be.revertedWith("E101")
+		})
+
+		it("A ballot should last more than 10 seconds", async function () {
+			const startDate = Math.floor(Date.now() / 1000) + 12
+			const endDate = Math.floor(Date.now() / 1000) + 21
+
+			await expect(elektonUser1.createBallot(ballotId, smt.root, startDate, endDate)).to.be.revertedWith("E102")
+		})
+
 		it("User with id = 1 should create a ballot of 30s with 4 voters", async function () {
-			const elektonUser = elekton.connect(accounts[1].signer)
-			const ballotId = 12n
-			const smt = await getSmt(accounts.slice(1, 5).map((account) => account.voter.publicKey))
-			const startDate = Math.floor(Date.now() / 1000) + 10
+			const startDate = Math.floor(Date.now() / 1000) + 12
 			const endDate = Math.floor(Date.now() / 1000) + 30
 
-			await expect(elektonUser.createBallot(ballotId, smt.root, startDate, endDate))
+			await expect(elektonUser1.createBallot(ballotId, smt.root, startDate, endDate))
 				.to.emit(elekton, "BallotCreated")
 				.withArgs(ballotId)
 		})
